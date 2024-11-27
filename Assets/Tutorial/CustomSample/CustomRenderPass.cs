a425de8672e2e77cd2aeab88f0d9b51489a37e03
zhengwenhao <zhengwenhaomail@gmail.com>
2024-11-27 13:08:59 +0800

    Custom Sample Code

---
+++ Assets/Tutorial/CustomSample/CustomRenderPass.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+
+public class CustomRenderPass : ScriptableRenderPass
+{
+    private class CustomPassData
+    {
+
+    }
+
+    public void Init()
+    {
+        profilingSampler = new ProfilingSampler("CustomRenderPass");
+        renderPassEvent = RenderPassEvent.BeforeRenderingOpaques;
+    }
+
+    public void Setup()
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+    {
+        base.RecordRenderGraph(renderGraph, frameData);
+
+        using (var builder = renderGraph.AddRasterRenderPass<CustomPassData>(passName, out CustomPassData data))
+        {
+            //passdata
+            //data.xx = 
+
+            //read
+            //builder.UseTexture  builder.UseRendererList
+
+            //write
+            //builder.SetRenderAttachment 
+
+            //process
+            builder.SetRenderFunc<CustomPassData>((CustomPassData data, RasterGraphContext context) => ExecutePass(data, context));
+        }
+    }
+
+    private void ExecutePass(CustomPassData data, RasterGraphContext context)
+    {
+        //process
+        //Blitter.blit  context.cmd.DrawMesh 
+    }
+}
---
+++ Assets/Tutorial/CustomSample/CustomRendererFeature.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class CustomRendererFeature : ScriptableRendererFeature
+{
+    private CustomRenderPass renderPass;
+
+
+    public override void Create()
+    {
+        renderPass = new CustomRenderPass();
+        renderPass.Init();
+    }
+
+    public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
+    {
+        renderPass.Setup();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        renderPass.Dispose();
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(renderPass);
+    }
+}

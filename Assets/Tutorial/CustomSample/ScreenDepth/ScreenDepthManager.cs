5cc263bbd9fc1619d687d34595a77f85f81482c0
zhengwenhao <zhengwenhaomail@gmail.com>
2024-11-27 15:37:56 +0800

    Custom Dem 01: Screen Depth

---
+++ Assets/Tutorial/CustomSample/ScreenDepth/ScreenDepthManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ScreenDepthManager : MonoBehaviour
+{
+    private ScreenDepthRenderPass renderPass;
+
+    private void OnEnable()
+    {
+        Create();
+    }
+
+    private void OnDisable()
+    {
+        Dispose();
+    }
+
+    public void Create()
+    {
+        renderPass = new ScreenDepthRenderPass();
+        renderPass.Init();
+
+        RenderPipelineManager.beginCameraRendering += SetupRenderPass;
+    }
+
+    public void Dispose()
+    {
+        renderPass.Dispose();
+
+        RenderPipelineManager.beginCameraRendering -= SetupRenderPass;
+    }
+
+    public void SetupRenderPass(ScriptableRenderContext context, Camera cam)
+    {
+        renderPass.Setup();
+        cam.GetUniversalAdditionalCameraData().scriptableRenderer.EnqueuePass(renderPass);
+    }
+}
---
+++ Assets/Tutorial/CustomSample/ScreenDepth/ScreenDepthRenderPass.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+
+public class ScreenDepthRenderPass : ScriptableRenderPass
+{
+    private Material material;
+
+    private class CustomPassData
+    {
+        internal TextureHandle screenColor;
+        internal Material overrideMaterial;
+    }
+
+    public void Init()
+    {
+        profilingSampler = new ProfilingSampler("ScreenDepthRenderPass");
+        renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+
+        material = new Material(Shader.Find("Shader Graphs/SG_ScreenDepth"));
+    }
+
+    public void Setup()
+    {
+    }
+
+    public void Dispose()
+    {
+        if (Application.isPlaying)
+        {
+            Object.Destroy(material);
+        }
+        else
+        {
+            Object.DestroyImmediate(material);
+        }
+    }
+
+    public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+    {
+        using (var builder = renderGraph.AddRasterRenderPass<CustomPassData>(passName, out CustomPassData data))
+        {
+            //passdata
+            data.screenColor = frameData.Get<UniversalResourceData>().activeColorTexture;
+            data.overrideMaterial = material;
+
+            //read
+            //builder.UseTexture(data.screenColor);
+
+            //write
+            builder.SetRenderAttachment(data.screenColor, 0, AccessFlags.ReadWrite);
+
+            //process
+            builder.SetRenderFunc<CustomPassData>((CustomPassData data, RasterGraphContext context) => ExecutePass(data, context));
+        }
+    }
+
+    private void ExecutePass(CustomPassData data, RasterGraphContext context)
+    {
+        //process
+        Blitter.BlitTexture(context.cmd, data.screenColor, new Vector4(1, 1, 0, 0), data.overrideMaterial, 0);
+    }
+}
